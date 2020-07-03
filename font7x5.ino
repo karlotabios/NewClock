6992516ff6c2bc6b3a75edbddfd4556f20b28802
Karlo Tabios <karlotabios@gmail.com>
2020-07-04 00:53:21 +0800

    Shortened kerning via 'kern--' in font7x5.ino. Noticing scrollText alternating because of LogoCount

--- font7x5.ino
+++ font7x5.ino
@@ -826,6 +826,7 @@ char LoadColumnBuffer(char ascii)
 	if (ascii >= 0x20 && ascii <= 0x7f)
 	{
 		kern = pgm_read_byte_near(font7x5_kern + (ascii - 0x20));
+		kern--;
 #if defined(ESP8266)
 		int offset = font7x5_offset[ascii - 0x20];
 #else
